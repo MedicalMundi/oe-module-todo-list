fd3979a9179dadd934bd790fbda88e272f6a502c
zero <teclaizerai@gmail.com>
2020-06-05 00:18:42 +0200

    Improve Description class and test.

--- todo-list/src/Domain/Todo/Description.php
+++ todo-list/src/Domain/Todo/Description.php
@@ -5,11 +5,24 @@ namespace MedicalMundi\TodoList\Domain\Todo;
 
 final class Description
 {
+    public const MIN_LENGHT = 5;
+    public const MAX_LENGHT = 1500;
+
+    /** @var string  */
     private $value;
 
     public function __construct(string $value)
     {
+        if (strlen($value) < self::MIN_LENGHT || strlen($value) > self::MAX_LENGHT) {
+            throw new \InvalidArgumentException(sprintf('Description should be min %s char and max %s char.', self::MIN_LENGHT, self::MAX_LENGHT));
+        }
+
         $this->value = $value;
+    }
+
+    public static function fromString(string $description): Description
+    {
+        return new self($description);
     }
 
     public function value(): string
@@ -27,8 +40,8 @@ final class Description
         return $this->value;
     }
 
-    public static function fromString(string $description): Description
+    public function equals(Description $other): bool
     {
-        return new self($description);
+        return $this->value === $other->value;
     }
 }
---
+++ todo-list/tests/Unit/Todo/DescriptionTest.php
@@ -0,0 +1,77 @@
+<?php declare(strict_types=1);
+
+
+namespace MedicalMundi\TodoList\Tests\Unit\Todo;
+
+use MedicalMundi\TodoList\Domain\Todo\Description;
+use PHPUnit\Framework\TestCase;
+
+class DescriptionTest extends TestCase
+{
+    private const VALID_DESCRIPTION = 'A valid description';
+    private const INVALID_DESCRIPTION = '';
+
+    /**
+     * @test
+     * @doesNotPerformAssertions
+     */
+    public function can_be_created(): void
+    {
+        Description::fromString(self::VALID_DESCRIPTION);
+    }
+
+    /** @test */
+    public function should_return_value(): void
+    {
+        $description = Description::fromString(self::VALID_DESCRIPTION);
+
+        self::assertEquals(self::VALID_DESCRIPTION, $description->value());
+    }
+
+    /** @test */
+    public function should_return_value_as_string(): void
+    {
+        $description = Description::fromString(self::VALID_DESCRIPTION);
+
+        self::assertEquals(self::VALID_DESCRIPTION, $description->toString());
+        self::assertEquals(self::VALID_DESCRIPTION, $description->__toString());
+    }
+
+    /** @test */
+    public function can_be_compared(): void
+    {
+        $first = Description::fromString(self::VALID_DESCRIPTION);
+        $second = Description::fromString('A second description');
+        $copyOfFirst = Description::fromString(self::VALID_DESCRIPTION);
+
+        self::assertFalse($first->equals($second));
+        self::assertTrue($first->equals($copyOfFirst));
+        self::assertFalse($second->equals($copyOfFirst));
+    }
+
+    /** @test */
+    public function short_values_should_throw_exception(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage(sprintf('Description should be min %s char and max %s char.', Description::MIN_LENGHT, Description::MAX_LENGHT));
+
+        Description::fromString(self::INVALID_DESCRIPTION);
+    }
+
+    /** @test */
+    public function long_values_should_throw_exception(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage(sprintf('Description should be min %s char and max %s char.', Description::MIN_LENGHT, Description::MAX_LENGHT));
+
+        $longDescription = str_repeat('x', (int) (Description::MAX_LENGHT + 1));
+
+        Description::fromString($longDescription);
+    }
+
+    /** @test */
+    public function invalid_values_should_throw_exception(): void
+    {
+        self::markTestIncomplete('Add dataprovider, check invalid char, sql injection, ecc..');
+    }
+}
