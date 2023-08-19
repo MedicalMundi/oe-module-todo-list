b35e641a9b394c39193fb942f7686ee6a604fac8
zerai <teclaizerai@gmail.com>
2023-08-19 14:49:52 +0200

    Cleanup monolog conf.

--- src/Config/monolog.php
+++ src/Config/monolog.php
@@ -7,12 +7,20 @@ use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\Reference;
 
 if ((new IsModuleStandAlone())()) {
-    // Logging in module 'var' directory
+    /**
+     * Logging in module 'var' directory when the module
+     * is executed as stand alone mode
+     */
     $container->register(StreamHandler::class, StreamHandler::class)
         ->addArgument(__DIR__ . '/../../var/log/module.log');
 } else {
-    // Logging in openemr 'var' directory
-    // TODO: change path, use openemr var dir
+    /**
+     * Logging in epenemr 'Documents' directory when the module
+     * is executed inside openemr
+     * TODO: change path, use openemr Documents dir
+     *      or change module folder permission after installation
+     *      chown apache:root -R interface/modules/custom_modules/oe-module-todo-list/
+     */
     $container->register(StreamHandler::class, StreamHandler::class)
         ->addArgument(__DIR__ . '/../../module.log');
 }
