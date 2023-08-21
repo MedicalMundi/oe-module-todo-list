086212b8b8390fc42a7a4d6e08324d644c831d99
zerai <teclaizerai@gmail.com>
2023-08-21 14:22:21 +0200

    Set Module::MODULE_VERSION to @dev.

--- src/Module.php
+++ src/Module.php
@@ -19,7 +19,7 @@ class Module implements ContainerInterface, RequestHandlerInterface
 {
     public const MODULE_NAME = 'ToDo List';
 
-    public const MODULE_VERSION = 'v0.1.4';
+    public const MODULE_VERSION = '@dev';
 
     public const MODULE_SOURCE_CODE = 'https://github.com/MedicalMundi/oe-module-todo-list';
 
