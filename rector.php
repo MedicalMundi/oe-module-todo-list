c1afd3eac72fcf32105a3a001b2ca42a1220179d
zerai <teclaizerai@gmail.com>
2024-01-16 19:17:35 +0100

    Update rector config.

--- rector.php
+++ rector.php
@@ -6,11 +6,11 @@ use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-        __DIR__ . '/config',
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
-        __DIR__ . '/todo-list/src',
-        __DIR__ . '/todo-list/tests',
+        //__DIR__ . '/config',
+        __DIR__ . '/src/Adapter',
+        //__DIR__ . '/tests',
+        //__DIR__ . '/todo-list/src',
+        //__DIR__ . '/todo-list/tests',
     ]);
 
     $rectorConfig->skip([
@@ -19,13 +19,13 @@ return static function (RectorConfig $rectorConfig): void {
         __DIR__ . '/tools',
     ]);
 
-    $rectorConfig->phpVersion(PhpVersion::PHP_74);
+    $rectorConfig->phpVersion(PhpVersion::PHP_81);
 
     // register a single rule
     //$rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
 
     // define sets of rules
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_80,
+        LevelSetList::UP_TO_PHP_81,
     ]);
 };
