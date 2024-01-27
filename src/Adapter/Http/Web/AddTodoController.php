595e02de68c2375f9dc75c28026afbb83fa9056a
zerai <teclaizerai@gmail.com>
2024-01-27 11:21:04 +0100

    Improve AddTodoController|template.

--- src/Adapter/Http/Web/AddTodoController.php
+++ src/Adapter/Http/Web/AddTodoController.php
@@ -3,10 +3,12 @@
 namespace OpenEMR\Modules\MedicalMundiTodoList\Adapter\Http\Web;
 
 use Ecotone\Modelling\CommandBus;
+use MedicalMundi\TodoList\Application\Domain\Todo\Command\PostTodo;
 use Nyholm\Psr7\Factory\Psr17Factory;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Log\LoggerInterface;
+use Ramsey\Uuid\Uuid;
 use Twig\Environment;
 
 class AddTodoController
@@ -20,6 +22,28 @@ class AddTodoController
 
     public function __invoke(ServerRequestInterface $request): ResponseInterface
     {
+        if ($request->getMethod() === 'POST' && null !== $request->getParsedBody()) {
+            $form = $request->getParsedBody();
+
+            $command = new PostTodo(Uuid::uuid4()->toString(), $form['title']);
+
+            $result = $this->commandBus->send($command);
+
+            if (Uuid::isValid($result)) {
+                // TODO: add session + flash message feature
+                //$this->session->getFlash()->add('success', 'Todo saved!');
+
+                //TODO: perform a redirect to todo details page
+            } else {
+                // TODO: add session + flash message feature
+                //$this->session->getFlash()->add('error', 'Error');
+
+                // TODO: show error and form with data
+            }
+
+            return $this->render('todo/new.html.twig', []);
+        }
+
         return $this->render('todo/new.html.twig', []);
     }
 
